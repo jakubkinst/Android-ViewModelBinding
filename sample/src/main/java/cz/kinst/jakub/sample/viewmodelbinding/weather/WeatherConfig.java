ff4dcdef3111a5fd26a2fa740a7e628a3fc7c4bb
Jakub Kinst <jakub@kinst.cz>
2015-12-07 11:34:38 +0100

    Updated README and sample project

--- sample/src/main/java/cz/kinst/jakub/sample/viewmodelbinding/weather/WeatherConfig.java
+++
@@ -1,8 +0,0 @@
-package cz.kinst.jakub.sample.viewmodelbinding.weather;
-
-/**
- * Created by jakubkinst on 10/11/15.
- */
-public class WeatherConfig {
-	public static final String WEATHER_APP_ID = "2de143494c0b295cca9337e1e96b00e0";
-}
