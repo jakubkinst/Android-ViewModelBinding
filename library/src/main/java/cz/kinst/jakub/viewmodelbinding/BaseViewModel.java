139c594706a345b35b6fc4639bc597ceaff2566d
Jakub Kinst <jakub@kinst.cz>
2015-12-16 16:31:58 +0100

    v 0.5 changes

--- library/src/main/java/cz/kinst/jakub/viewmodelbinding/BaseViewModel.java
+++ library/src/main/java/cz/kinst/jakub/viewmodelbinding/BaseViewModel.java
@@ -4,102 +4,139 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.databinding.BaseObservable;
+import android.graphics.drawable.Drawable;
+import android.support.annotation.ColorRes;
+import android.support.annotation.DrawableRes;
 import android.support.annotation.Nullable;
+import android.support.annotation.StringRes;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 
 
 /**
  * Created by jakubkinst on 10/11/15.
  */
 public class BaseViewModel<T> extends BaseObservable {
-    @Nullable
-    private ViewInterface<T> mView;
-    private String mUniqueIdentifier;
+	@Nullable
+	private ViewInterface<T> mView;
+	private String mUniqueIdentifier;
 
 
-    public BaseViewModel() {
-    }
+	public BaseViewModel() {
+	}
 
 
-    public void onResume() {
+	public void onResume() {
 
-    }
-
-
-    public void onPause() {
-
-    }
+	}
 
 
-    public void bindView(ViewInterface viewInterface) {
-        mView = viewInterface;
-    }
+	public void onPause() {
+
+	}
 
 
-    @SuppressWarnings("EmptyMethod")
-    public void onViewDetached(boolean finalDetachment) {
-        mView = null;
-    }
+	public void bindView(ViewInterface viewInterface) {
+		mView = viewInterface;
+	}
 
 
-    @SuppressWarnings("EmptyMethod")
-    public void onViewAttached(boolean firstAttachment) {
-    }
+	@SuppressWarnings("EmptyMethod")
+	public void onViewDetached(boolean finalDetachment) {
+		mView = null;
+	}
 
 
-    @SuppressWarnings("EmptyMethod")
-    public void onModelRemoved() {
-        mView = null;
-    }
+	@SuppressWarnings("EmptyMethod")
+	public void onViewAttached(boolean firstAttachment) {
+	}
 
 
-    @Nullable
-    public ViewInterface<T> getView() {
-        return mView;
-    }
+	@SuppressWarnings("EmptyMethod")
+	public void onModelRemoved() {
+		mView = null;
+	}
 
 
-    @Nullable
-    public Context getContext() {
-        if (mView == null) return null;
-        else return mView.getContext();
-    }
+	@Nullable
+	public ViewInterface<T> getView() {
+		return mView;
+	}
 
 
-    @Nullable
-    public Activity getActivity() {
-        if (mView == null) return null;
-        else return mView.getActivity();
-    }
+	@Nullable
+	public Context getContext() {
+		if(mView == null) return null;
+		else return mView.getContext();
+	}
 
 
-    public boolean hasViewAttached() {
-        return mView != null;
-    }
+	@Nullable
+	public Activity getActivity() {
+		if(mView == null) return null;
+		else return mView.getActivity();
+	}
 
 
-    /**
-     * @return An app unique identifier for the current viewmodel instance (will be kept during orientation
-     * change). This identifier will be reset in case the corresponding activity is killed.
-     */
-    public String getUniqueIdentifier() {
-        return mUniqueIdentifier;
-    }
+	public boolean hasViewAttached() {
+		return mView != null;
+	}
 
-    void setUniqueIdentifier(String uniqueIdentifier) {
-        mUniqueIdentifier = uniqueIdentifier;
-    }
 
-    public void startActivityForResult(Intent intent, int requestCode) {
-        if (!hasViewAttached()) return;
-        if (mView instanceof Activity) {
-            ((Activity) mView).startActivityForResult(intent, requestCode);
-        } else if (mView instanceof Fragment) {
-            ((Fragment) mView).startActivityForResult(intent, requestCode);
-        }
-    }
+	/**
+	 * @return An app unique identifier for the current viewmodel instance (will be kept during orientation
+	 * change). This identifier will be reset in case the corresponding activity is killed.
+	 */
+	public String getUniqueIdentifier() {
+		return mUniqueIdentifier;
+	}
 
-    @SuppressWarnings("EmptyMethod")
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-    }
+
+	void setUniqueIdentifier(String uniqueIdentifier) {
+		mUniqueIdentifier = uniqueIdentifier;
+	}
+
+
+	public void startActivityForResult(Intent intent, int requestCode) {
+		if(!hasViewAttached()) return;
+		if(mView instanceof Activity) {
+			((Activity) mView).startActivityForResult(intent, requestCode);
+		} else if(mView instanceof Fragment) {
+			((Fragment) mView).startActivityForResult(intent, requestCode);
+		}
+	}
+
+
+	@SuppressWarnings("EmptyMethod")
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+	}
+
+
+	protected T getBinding() {
+		if(hasViewAttached())
+			return getView().getBinding();
+		else
+			return null;
+	}
+
+
+	protected String getString(@StringRes int resource) {
+		if(getContext() == null)
+			return null;
+		return getContext().getString(resource);
+	}
+
+
+	protected Drawable getDrawable(@DrawableRes int resource) {
+		if(getContext() == null)
+			return null;
+		return ContextCompat.getDrawable(getContext(), resource);
+	}
+
+
+	protected int getColor(@ColorRes int resource) {
+		if(getContext() == null)
+			return -1;
+		return ContextCompat.getColor(getContext(), resource);
+	}
 }
