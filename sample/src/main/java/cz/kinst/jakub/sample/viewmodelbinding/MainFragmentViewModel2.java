6e206178bb46194cb5b3d94512951b5d695183cc
Artem Ufimtcev <alotxo@gmail.com>
2016-02-11 14:26:44 +0100

    Remove testing classes

--- sample/src/main/java/cz/kinst/jakub/sample/viewmodelbinding/MainFragmentViewModel2.java
+++
@@ -1,7 +0,0 @@
-package cz.kinst.jakub.sample.viewmodelbinding;
-
-/**
- * Created by astrdev on 11/02/16.
- */
-public class MainFragmentViewModel2 {
-}
--- sample/src/main/java/cz/kinst/jakub/sample/viewmodelbinding/NewMainActivity.java
+++
@@ -1,7 +0,0 @@
-package cz.kinst.jakub.sample.viewmodelbinding;
-
-/**
- * Created by astrdev on 10/02/16.
- */
-public class NewMainActivity {
-}
